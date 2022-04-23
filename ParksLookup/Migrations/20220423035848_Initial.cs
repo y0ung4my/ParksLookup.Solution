1f426336ac02970ce6af8fef98d8d848a791ff9a
Amy Young <youngamy1223@gmail.com>
2022-04-22 22:19:58 -0600

    add initial migration

---
+++ ParksLookup/Migrations/20220423035848_Initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParksLookup.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Parks",
+                columns: table => new
+                {
+                    ParkId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ParkSystem = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    State = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Name = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Activities = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parks", x => x.ParkId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Parks");
+        }
+    }
+}
---
+++ ParksLookup/Models/Park.cs
@@ -0,0 +1,11 @@
+namespace ParksLookup.Models
+{
+    public class Park
+    {
+        public int ParkId { get; set; }
+        public string ParkSystem { get; set; }
+        public string State { get; set; }
+        public string Name {get; set; }
+        public string Activities { get; set; }
+    }
+}
