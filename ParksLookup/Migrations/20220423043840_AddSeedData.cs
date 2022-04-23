a7b4eafd38df59a312920a8714517d72656c1368
Amy Young <youngamy1223@gmail.com>
2022-04-22 22:41:20 -0600

    add migration for SeedData

---
+++ ParksLookup/Migrations/20220423043840_AddSeedData.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParksLookup.Migrations
+{
+    public partial class AddSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "Activities", "Name", "ParkSystem", "State" },
+                values: new object[,]
+                {
+                    { 1, "hiking, horseback riding, snowshoeing", "Yellowstone", "National", "Wyoming" },
+                    { 2, "hiking, camping, astronomy", "Black Canyon of the Gunnison", "National", "Colorado" },
+                    { 3, "hiking, sight-seeing, living history", "Staunton State Park", "State", "Colorado" },
+                    { 4, "fishing, swimming, kayaking", "Huntsville State Park", "State", "Texas" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 4);
+        }
+    }
+}
--- ParksLookup/Models/ParksLookupContext.cs
+++ ParksLookup/Models/ParksLookupContext.cs
@@ -9,6 +9,16 @@ namespace ParksLookup.Models
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Park>().HasData(
+                new Park { ParkId = 1, ParkSystem = "National", State = "Wyoming", Name = "Yellowstone", Activities = "hiking, horseback riding, snowshoeing" },
+                new Park { ParkId = 2, ParkSystem = "National", State = "Colorado", Name = "Black Canyon of the Gunnison", Activities = "hiking, camping, astronomy" },
+                new Park { ParkId = 3, ParkSystem = "State", State = "Colorado", Name = "Staunton State Park", Activities = "hiking, sight-seeing, living history" },
+                new Park { ParkId = 4, ParkSystem = "State", State = "Texas", Name = "Huntsville State Park", Activities = "fishing, swimming, kayaking" }
+            );
+        }
+
         public DbSet<Park> Parks { get; set; }
     }
 }
