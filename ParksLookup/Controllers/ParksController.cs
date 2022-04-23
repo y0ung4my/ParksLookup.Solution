941258d429d3c57feef54d48130b0aa4c2da1bb5
Amy Young <youngamy1223@gmail.com>
2022-04-22 22:20:36 -0600

    add ParksController with Get and Post methods

---
+++ ParksLookup/Controllers/ParksController.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ParksLookup.Models;
+
+namespace ParksLookup.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ParksController : ControllerBase
+  {
+    private readonly ParksLookupContext _db;
+
+    public ParksController(ParksLookupContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/parks
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Park>>> Get()
+    {
+      return await _db.Parks.ToListAsync();
+    }
+
+    // GET: api/Parks/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Park>> GetPark(int id)
+    {
+        var park = await _db.Parks.FindAsync(id);
+
+        if (park == null)
+        {
+            return NotFound();
+        }
+
+        return park;
+    }
+
+    // POST api/parks
+    [HttpPost]
+    public async Task<ActionResult<Park>> Post(Park park)
+    {
+      _db.Parks.Add(park);
+      await _db.SaveChangesAsync();
+
+      return CreatedAtAction(nameof(GetPark), new { id = park.ParkId }, park);
+    }
+  }
+}
