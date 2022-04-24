5bb8142744d778ceb093a0ae7d2c2ec76ffbec7e
Amy Young <youngamy1223@gmail.com>
2022-04-23 20:08:58 -0600

    add Swagger

--- ParksLookup/Startup.cs
+++ ParksLookup/Startup.cs
@@ -24,11 +24,27 @@ namespace ParksLookup
             services.AddDbContext<ParksLookupContext>(opt =>
                 opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Version = "V1",
+                    Title = "Parks Lookup API",
+                    Description = "API for retrieving a list of national and state parks"
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Parks Lookup API V");
+                c.RoutePrefix = string.Empty;
+
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
